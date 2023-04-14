f02c34cc902a0b043dca49eb6389beeb4c0e70ee
zefass <96497097+zefass@users.noreply.github.com>
2023-04-14 11:16:57 +0300

    Delete dbus_thing.sh

--- dbus_thing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dbus-monitor --system "sender=org.freedesktop.NetworkManager, path=/org/freedesktop/NetworkManager, member=StateChanged" | sed -u -n 's/   uint32 20/\/usr\/bin\/python3 \/home\/zefass\/Desktop\/practice6\/stop.py/p; s/   uint32 70/\/usr\/bin\/python3 \/home\/zefass\/Desktop\/practice6\/start.py/p' | sh
