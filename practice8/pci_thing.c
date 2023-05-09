21215d942845d7c1968001dd40f85b1dda285cca
zefass <96497097+zefass@users.noreply.github.com>
2023-05-09 21:02:17 +0300

    Update pci_thing.c

--- practice8/pci_thing.c
+++ practice8/pci_thing.c
@@ -2,25 +2,43 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/ioctl.h>
+#include <linux/uaccess.h>
 
 #define PCI_VENDOR_ID 0x8086
 #define PCI_DEVICE_ID 0x100e
 
 #define DRIVER_NAME "pci_driver"
+#define DEVICE_NAME "pci_device" 
 #define IOCTL_MAC_ADDR _IOR('mac', 1, char*)
 
 MODULE_LICENSE("GPL");
 
+static int open(struct inode *, struct file *);
+static int release(struct inode *, struct file *); 
+static long ioctl(struct file *filep, unsigned int cmd, unsigned long arg);
 static int rtl8139_probe(struct pci_dev *dev, const struct pci_device_id *id);
 static void rtl8139_remove_one(struct pci_dev *dev);
 
+unsigned char port_addr[6];
+static char *port_Ptr; 
+static int major;
+void __iomem *mmio_addr;
+u8 *port_ptr;
 
 static struct pci_device_id rtl8139_pci_tbl[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID, PCI_DEVICE_ID) },
-	{0,}
+	{ 0, }
 };
 
-MODULE_DEVICE_TABLE(pci, rtl8139_pci_tbl);
+static struct file_operations pci_fops = {
+	.unlocked_ioctl = ioctl,
+	.open = open,
+	.release = release
+};
+
 
 static struct pci_driver rtl8139_pci_driver = {
 	.name = DRIVER_NAME,
@@ -29,88 +47,85 @@ static struct pci_driver rtl8139_pci_driver = {
 	.remove = rtl8139_remove_one
 };
 
-static int open(struct inode *inode, struct file *file);
-static int release(struct inode *inode, struct file *file);
-static long ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 
-static struct file_operations pci_fops = {
-	.open		=	open,
-	.release	=	release,
-	.unlocked_ioctl	=	ioctl
-};
+static int open(struct inode *inode, struct file *file) { 
+	port_Ptr = port_addr;
+	printk(KERN_INFO "Open port address: %pM\n", port_addr); 
+	try_module_get(THIS_MODULE);
 
-static int major;
-unsigned char port_addr[6];
-static struct class *cls;
-
-
-static int open(struct inode *inode, struct file *file) {
-    try_module_get(THIS_MODULE);
-    printk(KERN_INFO "Network_card: Character device opened\n");
-    return 0;
+	return 0;
 }
 
-static int release(struct inode *inode, struct file *file) {
-    module_put(THIS_MODULE);
-    printk(KERN_INFO "Network_card: Character device closed\n");
-    return 0;
+static int release(struct inode *inode, struct file *file) { 
+	module_put(THIS_MODULE);
+    	iounmap(mmio_addr);
+	return 0;
 }
 
-static long ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
-    switch (cmd) {
-        case IOCTL_MAC_ADDR:
-            if (copy_to_user((unsigned long __user *)arg, port_addr, 6) != 0) {
-                return -EFAULT;
-            }
-            break;
-        default:
-            return -ENOTTY;
+static long ioctl(struct file *filep, unsigned int cmd, unsigned long arg) {
+    	switch (cmd) {
+        	case IOCTL_MAC_ADDR:
+            		if (copy_to_user((char *)arg, port_addr, 6)) {
+                		return -EFAULT;
+            		}
+			break;
+        	default:
+            		return -ENOTTY;
+	}
+	return 0;
+}
+
+int get_port_address(struct pci_dev *dev, unsigned char *port_addr) {
+    mmio_addr = ioremap(pci_resource_start(dev, 0), pci_resource_len(dev, 0));
+    if (!mmio_addr) {
+        return -ENOMEM;
     }
+    port_ptr = (u8 *)(mmio_addr + 0x0000);
+    memcpy(port_addr, port_ptr, 6);
 
     return 0;
 }
 
+int rtl8139_probe(struct pci_dev *dev, const struct pci_device_id *id)
+{
+	
+	printk(KERN_INFO "Probe\n");
 
+	if (get_port_address(dev, port_addr)) {
+		printk(KERN_ERR "Unable to get port address\n");
+		return -EINVAL;
+	}
 
-static int rtl8139_init_module (void) {
+	printk(KERN_INFO "port address: %pM\n", port_addr);
+
+	major = register_chrdev(0, DEVICE_NAME, &pci_fops);
+	if (major < 0) { 
+		printk(KERN_ALERT "Registering char device failed with %d, do better!\n", major); 	
+		return major; 
+	} 
+	printk(KERN_INFO "try 'sudo mknod /dev/%s c %d 0'.\n", DEVICE_NAME, major); 
+
+	return 0;
+}
+
+void rtl8139_remove_one(struct pci_dev *dev)
+{
+	unregister_chrdev(major, DEVICE_NAME);	
+}
+
+int init_module(void)
+{	
+	printk(KERN_INFO "Init\n");
 	int pcireg = pci_register_driver(&rtl8139_pci_driver);
 	if (pcireg < 0) {
 		printk("Couldn't register pci driver. Do better!");
 		return pcireg;
 	}
-	major = register_chrdev(0, DRIVER_NAME, &pci_fops);
-	if (major < 0) {
-		printk(KERN_ALERT "Registering char device failed with %d, do better!\n", major);
-		return major;
-  	}
-  	printk(KERN_INFO "I was assigned major number %d.", major); 	
-  	cls = class_create(THIS_MODULE, DRIVER_NAME);
-    	device_create(cls, NULL, MKDEV(major, 0), NULL, DRIVER_NAME);
-    	
-  	return pcireg;
-}
-int rtl8139_probe(struct pci_dev *dev, const struct pci_device_id *id) {
-	int i;
-	u8 __iomem *mmio_addr;
-
-	mmio_addr = ioremap(pci_resource_start(dev, 0), pci_resource_len(dev, 0));
-    	if (!mmio_addr) {
-        	return -ENOMEM;
-    	}
-    	for (i = 0; i < 6; i++) {
-    		port_addr[i] = ioread8(mmio_addr + i);
-    		printk(KERN_INFO "Address number %d: %02x\n", i, mmio_addr[i]);
-    	}
-    	iounmap(mmio_addr);
-    	return 0;
+	return pcireg;
 }
 
-void rtl8139_remove_one(struct pci_dev *dev) {
-	unregister_chrdev(major,"MyPCI");
-}
-
-void cleanup_module(void) {
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Exit\n");
 	pci_unregister_driver(&rtl8139_pci_driver);
-	device_destroy(cls, MKDEV(major, 0));
-   	class_destroy(cls);
 }
